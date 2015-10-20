fix a bug related to the caculation of Kz. The formula I used was only valid for positive z.

SEP/SEP.py
@@ -64,7 +64,11 @@ elif pf.__dict__['BINARY'] in ['ELL1']:
     OMerr = 1./(1. + x**2) * er * x * 180./np.pi
 
 """Galactic acceleration for low z """
-Kz = lambda z:(2.27*z + 3.68*(1-np.exp(-4.31*z)) ) * 1.e-9 #Galactic acceleration in z direction (cm/s^2)
+#Kz = lambda z:(2.27*z + 3.68*(1-np.exp(-4.31*z)) ) * 1.e-9 #Galactic acceleration in z direction (cm/s^2)
+def Kz(z):
+    absz = np.abs(z)
+    sign = z/absz
+    return sign * (2.27*absz + 3.68*(1-np.exp(-4.31*absz)) ) * 1.e-9
 
 """Calculate the coordinate transfermation matrix"""
 T, GT = GetTransferMatrix(pf)#, paascnode)

SEP/SEP_ingrid.py
@@ -41,7 +41,11 @@ secperday = 24*3600
 R0 = solardist
 DRA = pos1713.ra.radians - GCpos.ra.radians
 DDec = pos1713.dec.radians - GCpos.dec.radians
-Kz = lambda z:(2.27*z + 3.68*(1-np.exp(-4.31*z)) ) * 1.e-9 #Galactic acceleration in z direction (cm/s^2)
+#Kz = lambda z:(2.27*z + 3.68*(1-np.exp(-4.31*z)) ) * 1.e-9 #Galactic acceleration in z direction (cm/s^2)
+def Kz(z):
+    absz = np.abs(z)
+    sign = z/absz
+    return sign * (2.27*absz + 3.68*(1-np.exp(-4.31*absz)) ) * 1.e-9
 
 
 def getKG(kr, zeta, z, sini, paascnode, om):

SEP/SEP_justin.py
@@ -38,7 +38,11 @@ OMerr = float(pf.OM[1])/180.*np.pi
 """Calculate the coordinate transfermation matrix"""
 T, GT = GetTransferMatrix(pf)
 """Galactic acceleration for low z """
-Kz = lambda z:(2.27*z + 3.68*(1-np.exp(-4.31*z)) ) * 1.e-9 #Galactic acceleration in z direction (cm/s^2)
+#Kz = lambda z:(2.27*z + 3.68*(1-np.exp(-4.31*z)) ) * 1.e-9 #Galactic acceleration in z direction (cm/s^2)
+def Kz(z):
+    absz = np.abs(z)
+    sign = z/absz
+    return sign * (2.27*absz + 3.68*(1-np.exp(-4.31*absz)) ) * 1.e-9
 
 
 def getKG(kr, zeta, z, sini, paascnode, om):